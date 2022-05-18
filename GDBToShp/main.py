cf1df4ad6cc10db353ac06f19b770c59dbeaf74e
xutinting <Xutingting178@outlook.com>
2022-05-18 16:24:13 +0800

    arcpy 练习

---
+++ GDBToShp/main.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import arcpy
+import os.path
+from arcpy import env
+from config import PATHS as paths
+from util import getGDBs
+
+[message,GDBs] = getGDBs()
+
+print(message,GDBs)
+# env.workspace = "C:/Users/pc/Desktop/Practice/GDBToShp/input"
+# in_features = "四川省生态保护红线.gdb"
+# out_feature_class = "C:/Users/pc/Desktop/Practice/GDBToShp/output/STBHHX_200.shp"
+# # 阿坝藏族羌族自治州-马尔康市
+# countyCode = 201
+# where_clause = '"FID" = ' + str(countyCode)
+# arcpy.Select_analysis(in_features,out_feature_class,where_clause)
