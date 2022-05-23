229b741c58c927f0a6dd04667ab014af3dcb9ac2
xutinting <Xutingting178@outlook.com>
2022-05-23 17:59:49 +0800

    list dataset function

--- GDBToShp/main.py
+++ GDBToShp/main.py
@@ -4,12 +4,17 @@ import os
 from config import PATHS
 from util import getGDBs,listFeatureDatasets,admin_codes
 
-GDBs = getGDBs()
 
 # 输入文件夹路径
 input_path = PATHS['input_path']
 # 输出文件夹路径
 output_path = PATHS['output_path']
+
+# read gdb file path
+gdblist = getGDBs()
+gdb = gdblist[0]
+
+
 
 for admin_code in admin_codes:
     # 创建目录
@@ -17,15 +22,11 @@ for admin_code in admin_codes:
     county = admin_code['County']
     county_code = admin_code['CountyCode']
     county_path = (output_path + '\\' + city + '\\' + county_code + county)
-    print(county_path)
-    break
     if not os.path.exists(county_path):
         os.makedirs( county_path )
     # 选择图斑
-    gdb = GDBs[0]
     [message, featuresDatasetsNames,gdbName] = listFeatureDatasets(gdb)
     in_features = gdb
     out_feature_class = county_path + "\\" + "STBHHX_2019.shp"
     where_clause = "行政区划代码 = " + county_code
     arcpy.Select_analysis(in_features,out_feature_class,where_clause)
-
--- GDBToShp/util.py
+++ GDBToShp/util.py
@@ -1,42 +1,77 @@
 # -*- coding: UTF-8 -*-
 import arcpy
 import os
+import sys
+import traceback
 from config import PATHS
 
 input_path = PATHS['input_path']
 
 def getGDBs():
-    filesNames = os.listdir(input_path)
+    dirlist = os.listdir(input_path)
 
-    GDBs = []
-    for fileName in filesNames:
-        extension = os.path.splitext(fileName)[1]
+    gdblist = []
+    for dir in dirlist:
+        extension = os.path.splitext(dir)[1]
 
-        if extension == '.gdb':
-            file = os.path.join(input_path, fileName)
-            GDBs.append(file)
+    if ".gdb" in dir: #如果路径名称中包含 .gdb 字符串
+        gdblist.append(input_path+"\\"+dir) #将GDB数据库的路径添加到gdblist列表中
 
-    return GDBs
+    return gdblist
+
+def getFormattedException():
+    excType, excValue, excTb = sys.exc_info()
+    formattedException = traceback.format_exception(excType, excValue, excTb)
+
+    return '\n'.join(formattedException)
+
 
 def listFeatureDatasets(gdb):
-    arcpy.env.workspace = gdb
+    featuresDatasetsExists = False
 
-    gdbName = os.path.splitext(os.path.basename(gdb))[0]
-    
-    featureDatasetsNames = arcpy.ListDatasets('*', 'Feature')
-    featuresDatasetsNames = []
-    for featureDatasetName in featureDatasetsNames:
-        dataset = os.path.join(gdb, featureDatasetName)
+    try:
+        arcpy.env.workspace = gdb
 
-        datasetType = arcpy.Describe(dataset).datasetType
+        gdbName = os.path.splitext(os.path.basename(gdb))[0]
 
-        if datasetType == 'FeatureDataset':
-            featuresDatasetsNames.append(featureDatasetName)
+        featureDatasetsNames = arcpy.ListDatasets('*', 'Feature')
 
-    message = 'The datasets from file geodatabase {} were successfully listed\n'.format(gdbName)
+        if len(featureDatasetsNames) == 0:
+            message = 'There is no dataset in the file geodatabase {}\n'.format(gdbName)
 
-    return [message, featuresDatasetsNames,gdbName]
+            return [False, featuresDatasetsExists, message, featureDatasetsNames]
 
+        featuresDatasetsExists = True
+
+        featuresDatasetsNames = []
+        for featureDatasetName in featureDatasetsNames:
+            dataset = os.path.join(gdb, featureDatasetName)
+
+            datasetType = arcpy.Describe(dataset).datasetType
+
+            if datasetType == 'FeatureDataset':
+                featuresDatasetsNames.append(featureDatasetName)
+
+        message = 'The datasets from file geodatabase {} were successfully listed\n'.format(gdbName)
+
+        return [True, featuresDatasetsExists, message, featuresDatasetsNames]
+
+    except:
+        formattedException = getFormattedException()
+
+        message = 'There was a problem while listing datasets.\nError:\n{}\n'.format(formattedException)
+
+        return [False, featuresDatasetsExists, message, '']
+
+
+def judge(gdb, field):
+    fields = arcpy.ListFields(gdb)
+    for fd in fields:
+        if fd.name == field:
+            if fd.length >= 32:
+                return 1
+            else:
+                return 0
 
 admin_codes = [
 { 'City' : '成都市', 'County' : '锦江区', 'CountyCode' : '510104' },
